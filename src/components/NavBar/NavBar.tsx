07d97b87c94aeb5e8b71020de09d7dba9c565557
FERNANDO HENRIQUE FERNANDES DA SILVA <f3rnandorj10@gmail.com>
2024-05-15 12:01:31 -0300

    Finish responsive navBar component.

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,28 +1,39 @@
 "use client";
 
 import { backgroundImage, utilsImages } from "@/assets";
-import { Image } from "@/components";
+import { Image, MARGIN_HORIZONTAL } from "@/components";
 import { SwipeButton, swipeButtonDataToMap } from "./components/SwipeButton";
+import { useIsMobileDevice } from "@/hooks";
+
 export function NavBar() {
+  const isMobile = useIsMobileDevice();
+
   return (
     <nav
-      className="w-full flex h-[15rem] justify-evenly items-center px-28
-      md:h-[20rem] 
-      lg:h-[10.5rem] 
-      "
+      className={`w-full flex flex-col h-[15rem] justify-evenly items-center 
+      md:h-[20rem] md:flex-row 
+      lg:h-[10.5rem] lg:flex-row lg:px-28
+      ${MARGIN_HORIZONTAL}
+      `}
       style={{ backgroundImage: `url(${backgroundImage.header.src})` }}
     >
-      <Image
-        alt=""
-        src={utilsImages.stamp.src}
-        height={60}
-        width={145}
-        className="pt-20"
-      />
+      {!isMobile && (
+        <Image
+          alt=""
+          src={utilsImages.stamp.src}
+          height={60}
+          width={145}
+          className="pt-20"
+        />
+      )}
 
       <Image alt="logo" src={utilsImages.logo} width={235} height={130} />
 
-      <div className="flex flex-col gap-1.5">
+      <div
+        className="flex justify-center w-full gap-1.5 
+        md:w-auto md:flex-col
+        lg:w-auto lg:flex-col"
+      >
         {swipeButtonDataToMap.map((button) => (
           <SwipeButton key={button.title} {...button} />
         ))}
--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -2,11 +2,14 @@ import { backgroundImage } from "@/assets";
 
 interface SectionProps extends React.HTMLAttributes<HTMLDivElement> {}
 
+export const MARGIN_HORIZONTAL = "px-6";
+
 export function Section({ ...divProps }: SectionProps) {
   const styles = {
-    sectionContainer: `max-w-[70rem] mx-auto flex flex-1 flex-col items-center
+    sectionContainer: `max-w-[70rem] flex flex-1 flex-col items-center 
      md:mt-14 md:mb-14 
-     lg:mt-20 lg:mb-20
+     lg:mt-20 lg:mb-20 lg:mx-auto 
+     ${MARGIN_HORIZONTAL}
   `,
   };
 
@@ -15,7 +18,7 @@ export function Section({ ...divProps }: SectionProps) {
       style={{
         backgroundImage: `url(${backgroundImage.section.src})`,
       }}
-      className={`-mt-1.5 -ml-1.5 bg-cover`}
+      className={`-mt-1.5 -ml-1.5 bg-cover `}
     >
       <div {...divProps} className={styles.sectionContainer} />
     </div>
